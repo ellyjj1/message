2efd99064811ebc9d9a334bcd826ddf75de0d9dc
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-06-10 16:51:36 +1200

    creat serializer viewset for chatroom

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,5 @@ from rest_framework.routers import DefaultRouter
 from chat.viewsets import ChatRoomViewSet
 
 router = DefaultRouter()
-
 router.register( r'chatroom', ChatRoomViewSet, basename='chatroom')
 urlpatterns = router.urls
