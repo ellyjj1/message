820fd0eb365374abde0f8ab7d53efb211951a099
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-07-01 16:32:24 +1200

    add view and test it

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+
+from chat.views import sumNumbers, sumNumbersView
 from chat.viewsets import ChatRoomViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register( r'chatroom', ChatRoomViewSet, basename='chatroom')
 router.register( r'message', MessageViewSet, basename='message')
 urlpatterns = router.urls
-
+urlpatterns += [
+    path('sum_numbers/', sumNumbersView, name='sum_numbers'),
+]
