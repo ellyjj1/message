4a153fa147d2ffb21ff58cc3d7f3fbead0d60f87
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-07-03 22:32:33 +1200

    when the content type is different use different function

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -17,8 +19,9 @@ def sumNumbers(start_num, end_num):
 def sumNumbersView(request):
     if request.method == 'POST':
         # 增加调试记录：
-        print("Request Headers:", request.headers)
-        print("Request Data:", request.data)
+        print("!!!Request Headers!!!:", request.headers)
+        print("!!!Request Data!!!:", request.data)
+        print("!!!Request POST!!!:", request.POST)
 
         # V1：以下为替代为get方法
         # start_num = request.data['start_num']
@@ -29,12 +32,28 @@ def sumNumbersView(request):
         end_num = request.data.get('end_num')
 
         # V3增加判断是否Json
+        # if request.content_type == 'application/x-www-form-urlencoded':
+        #     start_num = request.POST.get('start_num')
+        #     end_num = request.POST.get('end_num')
+        # else:
+        #     start_num = request.data.get('start_num')
+        #     end_num = request.data.get('end_num')
+
+        # 根据 Content-Type 解析请求数据
         if request.content_type == 'application/x-www-form-urlencoded':
-            start_num = request.POST.get('start_num')
-            end_num = request.POST.get('end_num')
+            # 处理 URL 编码的数据
+            try:
+                body = json.loads(list(request.POST.keys())[0])
+                start_num = body.get('start_num')
+                end_num = body.get('end_num')
+            except (json.JSONDecodeError, IndexError):
+                return Response({'error': 'Invalid form data'}, status=400)
         else:
+            # 处理 JSON 编码的数据
             start_num = request.data.get('start_num')
             end_num = request.data.get('end_num')
+
+
 
         # 已下到result上面为新增
         if start_num is None or end_num is None:
